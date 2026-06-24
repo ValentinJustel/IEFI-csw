1be00ffc1aeaff8bf5bae95c1c0be967094dc61b
ValentinJustel <justelvalentin@iresm.edu.ar>
2026-06-24 20:51:04 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,11 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
-  // Verificamos si existe la cookie de sesión de Better Auth
-  const sessionToken = request.cookies.get("better-auth.session_token");
-  // Si intenta entrar al dashboard y NO tiene token, lo enviamos al login
+  const sessionToken =
+    request.cookies.get("better-auth.session_token") ||
+    request.cookies.get("__Secure-better-auth.session_token");
+
   if (request.nextUrl.pathname.startsWith("/dashboard") && !sessionToken) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
+
   return NextResponse.next();
 }
-export const config = {
-  matcher: ["/dashboard/:path*"],
-};
